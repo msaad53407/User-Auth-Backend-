e003528a98d87e61735ffba15db55acd6c23654b
Muhammad Saad <pubg53407@gmail.com>
2023-08-31 15:36:22 +0500

    Setting Back CORS

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const { mongodb, MongoClient, ServerApiVersion } = require('mongodb')
 const ObjectId = require('mongodb').ObjectId;
-// const cors = require('cors');
+const cors = require('cors');
 
 require('dotenv').config()
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 
 const port = process.env.PORT || 5000;
